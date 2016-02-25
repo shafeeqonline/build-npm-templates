11d4ca9825dafe55af6d8fe77a202ffaf763cc5a
shafeeqonline <shafeeq.rahman01@gmail.com>
2016-02-25 09:47:13 +0530

    One file to build them all

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ function buildNpmTemplates(options) {
     var buildfolder = path.join(buildfolder, '../templates');
     var createbuild = path.join(process.cwd(), buildfolder);
     function mkdir(path, root) {
-  	    var dirs = path.split('/'), dir = dirs.shift(), root = (root || '') + dir + '/';
-  	    try { fs.mkdirSync(root); }
-  	    catch (e) {
-  	        //dir wasn't made, something went wrong
-  	        if(!fs.statSync(root).isDirectory()) throw new Error(e);
-  	    }
-  	    return !dirs.length || mkdir(dirs.join('/'), root);
-  	}
-    if(process.platform == "win32" || process.platform == "win64"){
+                    var dirs = path.split('/'), dir = dirs.shift(), root = (root || '') + dir + '/';
+                    try { fs.mkdirSync(root); }
+                    catch (e) {
+                        //dir wasn't made, something went wrong
+                        if(!fs.statSync(root).isDirectory()) throw new Error(e);
+                    }
+                    return !dirs.length || mkdir(dirs.join('/'), root);
+                }
+    if(process.platform == "win32" || process.platform == "win64"){
     mkdir(process.cwd() + buildfolder.replace(/\\/g,"/"));
     var tenantParts = buildfolder.split("\\");
     }
@@ -26,9 +26,9 @@ function buildNpmTemplates(options) {
       mkdir(createbuild);
       var tenantParts = buildfolder.split("/");
     }
-    console.log(tenantParts[tenantParts.length - 2])
+    var appName =  tenantParts[tenantParts.length - 2];
     var nodepath = path.join(process.cwd(), '/node_modules');
-    var options = options ? options : tenantParts[tenantParts.length - 2]
+    var options = options ? options : appName ;
     if(options){
       var tenants = options.split(",");
       fs.readdir(nodepath, function(err, items) {
@@ -50,13 +50,32 @@ function buildNpmTemplates(options) {
           })
         })
       });
+      fs.readdir(path.join(process.cwd(), "app/", appName , "/components/"), function(err, items) {
+        items.forEach(function(npmcomponent, index){
+          tenants.forEach(function(tenantname){
+            if(npmcomponent.indexOf(tenantname+'-component-') > -1){
+              var templatepath = path.join(process.cwd(), "app/", appName , "/components/", npmcomponent + '/template/');
+              var componentname = npmcomponent.split('-')[2];
+              var destdir = path.join(process.cwd(), buildfolder, '/', componentname);
+              if (!fs.existsSync(destdir)){
+                  fs.mkdirSync(destdir);
+              }
+              ncp(templatepath, destdir, function (err) {
+               if (err) {
+                 return console.error(err);
+               }
+              });
+            }
+          })
+        })
+      });
     }
   };
 }
 
 buildNpmTemplates.prototype.apply = function(compiler, options) {
   var commonpath = path.resolve(compiler.options.output.publicPath,process.cwd());
-  var createfolder = compiler.options.output.publicPath.replace(commonpath,'');
+  var createfolder = compiler.options.output.publicPath.replace(commonpath,'');
   this.createFolders(createfolder);
 };
 
